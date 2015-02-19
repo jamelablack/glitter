b30ed9845f192adbbfc8a5154f21824885a6e1cd
jamelablack <jamela.black@gmail.com>
2015-02-19 10:38:37 -0500

    added user, statuses and barebones auth

---
+++ glitter/app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+
+		if @user.save
+			flash[:notice] = "You're now registered!"
+			redirect_to user_path(@user.username)
+		else
+			render :new
+		end
+	end
+
+
+
+
+	def show
+		@user = User.find_by username: params[:username]
+	end
+
+
+	private
+
+	def user_params
+		params.require(:user).permit!
+
+	end
+
+
+end
